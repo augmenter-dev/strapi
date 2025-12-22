de1267eadd5e98dba06aabc8804e3348591cede2
Julien <jjy.quere@gmail.com>
2025-12-22 16:22:51 +0100

    Disable gh disaptch

--- src/utils/github-dispatch.js
+++ src/utils/github-dispatch.js
@@ -2,20 +2,21 @@ const token = process.env.GITHUB_WEBHOOK_TOKEN; // PAT GitHub
 const apiUrl = `https://api.github.com/repos/Oltre-dev/oltre-news-front/actions/workflows/deploy.yml/dispatches`;
 
 module.exports = async (ref = "main") => {
-  await fetch(apiUrl, {
-    method: "POST",
-    headers: {
-      Accept: "application/vnd.github.v3+json",
-      "Content-Type": "text/plain; charset=utf-8",
-      Authorization: `Bearer ${token}`,
-      "User-Agent": "strapi5-webhook",
-    },
-    body: JSON.stringify({ ref }),
-  }).then((r) => {
-    if (!r.ok) {
-      strapi.log.error(
-        `GitHub workflow dispatch failed: ${r.status} ${r.statusText}`
-      );
-    }
-  });
+  return;
+  // await fetch(apiUrl, {
+  //   method: "POST",
+  //   headers: {
+  //     Accept: "application/vnd.github.v3+json",
+  //     "Content-Type": "text/plain; charset=utf-8",
+  //     Authorization: `Bearer ${token}`,
+  //     "User-Agent": "strapi5-webhook",
+  //   },
+  //   body: JSON.stringify({ ref }),
+  // }).then((r) => {
+  //   if (!r.ok) {
+  //     strapi.log.error(
+  //       `GitHub workflow dispatch failed: ${r.status} ${r.statusText}`
+  //     );
+  //   }
+  // });
 };
